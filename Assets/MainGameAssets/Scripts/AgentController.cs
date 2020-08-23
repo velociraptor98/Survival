31243e28b724cc73d95f9bed7621409d601b86ea
kunal <kunal1998doon@gmail.com>
2020-08-23 18:01:27 +0530

    Basic movement Implementation

---
+++ Assets/MainGameAssets/Scripts/AgentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private ApplyMovement movement;
+    [SerializeField] private PlayerMovement input;
+    void Start()
+    {
+        movement = GetComponent<ApplyMovement>();
+        input = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.HandleMovement(input.MovementInputVector);
+    }
+}
---
+++ Assets/MainGameAssets/Scripts/ApplyMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+
+public class ApplyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected CharacterController charaterController;
+    [SerializeField]private float movementSpeed;
+    [SerializeField]private float gravity;
+    [SerializeField]private float rotateSpeed;
+    protected Vector3 movementDirection = Vector3.zero;
+    protected float desRotationAngle = 0;
+    private int inputVerticalDirection = 0;
+    public void HandleMovement(Vector2 Input)
+    {
+        if(charaterController.isGrounded)
+        {
+            if(Input.y !=0)
+            {
+                if(Input.y>0)
+                {
+                    inputVerticalDirection = Mathf.CeilToInt(Input.y);
+                }
+                else
+                {
+                    inputVerticalDirection = Mathf.FloorToInt(Input.y);
+                }
+                movementDirection = Input.y * transform.forward * movementSpeed;    
+            }
+        }
+        else
+        {
+            movementDirection = Vector3.zero;
+        }
+    }
+    void Start()
+    {
+        charaterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection.y -= gravity;
+        charaterController.Move(movementDirection * Time.deltaTime * movementSpeed);
+    }
+}
